ad293926b3197d0aae0485fd499d45166ce9115c
Jake Snyder <jak3legacy@gmail.com>
2023-02-02 23:31:38 -0500

    implementing Tina CMS

---
+++ .tina/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "8ae7d692-4989-4a1b-8d18-1a060d23b925", // Get this from tina.io
+  token: "2e8cec94a49efed34430a8237e39e821ee79f245", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
